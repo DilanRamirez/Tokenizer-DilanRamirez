aaca12b209866a1ec633ab44abdbd8304153a538
DilanRamirez <drramirezra@miners.utep.edu>
2019-10-07 11:54:20 -0600

    README Updated

--- simpleUI.c
+++ simpleUI.c
@@ -1,63 +1,69 @@
 #include <stdio.h>
-#include "Tokenizer.h"
-#include "History_Functions.h"
+#include "history.h"
+#include "tokenizer.h"
 
-
-void interface(int answer, char *str){
-  char sentence[50];
-  char **tokens;
-  int id = 0;
-  List *list = init_history();
-
-  switch(answer){
-    case 1:{
-       getSentence(sentence);
-    break;
-    }
-
-    case 2:{
-      getSentence(sentence);
-       tokens = tokenize(str);
-       print_tokens(tokens);
-       free_tokens(tokens);
-       printf("\n");
-    break;
-    }
-    
-    case 3:{
-       add_history(list, str);
-       print_history(list);
-    break;
-    }
-      
-    case 4:{
-       printf("BYE\n");
-    break;
-    }
-  }
-}
 
 
 int main(){
-  int answer = 0;
+
+  int answer = 0, id = 0, option = 0;
   char sentence[50];
+  char **tokens, *str, *ansAux;
+  List *list = init_history();
+
   printf("---------WELCOME-------\n");
 
   do{
+    fflush(stdin);
     getSentence(sentence);
     printf("\nChoose and Option from Below:\n");
-    printf("1.Enter a new string\n");
-    printf("2.Tokenize a String\n");
-    printf("3.Print History\n");
-    printf("4.Exit\n");
-    do{
-      printf("\nYour Option: ");
-      scanf("%d", &answer);
-      if(answer<1 || answer >4)
-          printf("Incorrect, choose a valid option\n");
-          interface(answer, sentence);
-    }while(answer<0 || answer>4);
-  }while(answer!=4);
+    printf("1. Tokenize a String\n");
+    printf("2. History Options\n");
+    printf("3. Exit\n");
+    printf("\nYour Option: ");
+    fflush(stdin);
+    scanf(" %d", &answer);
+    printf("You selected: %d \n", answer);
 
+    switch(answer){
+      case 1:
+        add_history(list, sentence);
+        tokens = tokenize(sentence);
+        print_tokens(tokens);
+        free_tokens(tokens);
+        printf("\n");
+      break;
+
+      case 2:
+        fflush(stdin);
+        printf("Choose an Option: \n");
+        printf("1. Print History\n");
+        printf("2. Recall an Item by ID\n");
+        printf("3. Clean History\n");
+        add_history(list, sentence);
+        scanf(" %d", &option);
+
+        switch(option){
+          case 1:
+	     print_history(list);
+	     fflush(stdin);
+             break;
+          case 2:
+	     printf("Number of ID: ");
+	     scanf("%d", &id);
+	     get_history(list, id);
+             break;
+
+          case 3:
+	     free_history(list);
+             break; 
+	break;
+      }
+      
+      default:
+      break;
+    }
+  }while(answer!=3);
+  
   return 0;
 }
